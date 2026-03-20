6928b797537a31e1ec047d48a67d957f9381e091
GuoHan Zhao <zhaoguohan@kylinos.cn>
2026-03-20 14:30:16 +0800

    block/curl: free s->password in cleanup paths
    When password-secret is used, curl_open() resolves it with
    qcrypto_secret_lookup_as_utf8() and stores the returned buffer in
    s->password.
    
    Unlike s->proxypassword, s->password is not freed either in the open
    failure path or in curl_close(), so the resolved secret leaks once it
    has been allocated.
    
    Free s->password in both cleanup paths.
    
    Fixes: 1bff96064290 ('curl: add support for HTTP authentication parameters')
    Signed-off-by: GuoHan Zhao <zhaoguohan@kylinos.cn>
    Message-ID: <20260320063016.262954-1-zhaoguohan_salmon@163.com>
    Reviewed-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    (cherry picked from commit 51fc8443c122fedf4d4891bbc3a1ff25dd8bacdf)
    Signed-off-by: Michael Tokarev <mjt@tls.msk.ru>

--- block/curl.c
+++ block/curl.c
@@ -865,6 +865,7 @@ out_noclean:
     g_free(s->cookie);
     g_free(s->url);
     g_free(s->username);
+    g_free(s->password);
     g_free(s->proxyusername);
     g_free(s->proxypassword);
     if (s->sockets) {
@@ -976,6 +977,7 @@ static void curl_close(BlockDriverState *bs)
     g_free(s->cookie);
     g_free(s->url);
     g_free(s->username);
+    g_free(s->password);
     g_free(s->proxyusername);
     g_free(s->proxypassword);
 }
